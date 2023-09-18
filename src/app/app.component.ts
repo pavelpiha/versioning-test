637bea59294be6d40220671319616e662afb5ac8
ppiha <ppiha@infoblox.com>
2023-09-18 23:49:40 +0200

    feat: title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'versioning-test';
+  title = 'versioning-test title changed';
 }
